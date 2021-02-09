456e0145c97b86663e44e7b56c9486b1448902f3
Trevor Nkuna <trevornkuna7@gmail.com>
2021-02-09 18:23:00 +0200

    Task010 fix

--- Task010/src/com/example/Task010/CommonCharacters.java
+++ Task010/src/com/example/Task010/CommonCharacters.java
@@ -1,20 +1,21 @@
 package com.example.Task010;
 import java.util.Locale;
 import java.util.Scanner;
+import  java.lang.StringBuilder;
 
 public class CommonCharacters {
     public static void main(String[] args){
 
-        check_for_common_chars("house","computers");
+        check_for_common_chars("look","book");
 
     }
     static void check_for_common_chars(String input1,String input2){
-        String firstString = input1.toLowerCase();
-        String secondString = input2.toLowerCase();
-        String primaryString, secondaryString;
-        String sep =", ";
+        String firstString = input1.toLowerCase(), secondString = input2.toLowerCase();
+        String primaryString, secondaryString, outString="", tempString ="", sep =", ";
         System.out.print("Common letters: ");
-        if(firstString.length() > secondString.length()){
+
+        // check which string is longer and assign it as primary string
+        if(firstString.length() > secondString.length() || firstString.length() == secondString.length()){
             primaryString = firstString;
             secondaryString = secondString;
         }
@@ -22,18 +23,26 @@ public class CommonCharacters {
             primaryString = secondString;
             secondaryString = firstString;
         }
-        for(int x = 0; x < primaryString.length();x++){
+
+        //For loop to find the common characters in the given string
+        for(int x = 0; x < primaryString.length(); x++){
             if(x == primaryString.length()-1)
             {
                 sep ="";
             }
+            String strr = Character.toString(primaryString.charAt(x));
+
             for (int y = 0 ; y < secondaryString.length(); y++){
-                if(primaryString.charAt(x) == secondaryString.charAt(y)){
+
+                String strr1 = Character.toString(secondaryString.charAt(y));
+                //To eliminate duplicates, exit loop if you find the first occurance
+                if(strr.equals(strr1)){
                     System.out.print(primaryString.charAt(x)+sep);
+                    y += secondaryString.length()-1;
                 }
             }
-        }
 
+        }
 
     }
 }
